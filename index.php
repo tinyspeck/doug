576a60bcbfba6039a0e9ab2b2f9ccf6e4d2a4e5b
Cal Henderson <cal@iamcal.com>
2010-08-16 17:47:01 +0000

    added search to bug indexes
    git-svn-id: svn+ssh://svn/var/svn/code.www/trunk/bugs@23509 f6f90d3d-aecf-4eb7-b537-f01840bbe213

--- index.php
+++ index.php
@@ -6,6 +6,81 @@
 	include('include/init.php');
 
 
+
+	#
+	# search filter?
+	#
+
+	$search_extra = 1;
+	$s = '';
+
+	if ($_GET[s]){
+
+		$s = '"'.$_GET[s].'"';
+
+		$terms = search_split_terms($_GET[s]);
+		$terms_db = search_db_escape_terms($terms);
+
+		$parts_bugs = array();
+		$parts_notes = array();
+
+		foreach ($terms_db as $term_db){
+			$parts_bugs[] = "title RLIKE '$term_db'";
+			$parts_notes[] = "note RLIKE '$term_db'";
+		}
+
+		$parts_bugs = implode(' AND ', $parts_bugs);
+		$parts_notes = implode(' AND ', $parts_notes);
+
+		$ids1 = db_fetch_all("SELECT id FROM bugs WHERE $parts_bugs");
+		$ids2 = db_fetch_all("SELECT bug_id FROM notes WHERE $parts_notes");
+
+		$ids = array(0);
+
+		foreach ($ids1 as $row){ $ids[$row[id]] = 1; }
+		foreach ($ids2 as $row){ $ids[$row[bug_id]] = 1; }
+
+		$search_extra = " id IN (".implode(',', array_keys($ids)).")";
+	}
+
+	function search_split_terms($terms){
+
+		$terms = preg_replace("/\"(.*?)\"/e", "search_transform_term('\$1')", $terms);
+		$terms = preg_split("/\s+|,/", $terms);
+
+		$out = array();
+
+		foreach($terms as $term){
+
+			$term = preg_replace("/\{WHITESPACE-([0-9]+)\}/e", "chr(\$1)", $term);
+			$term = preg_replace("/\{COMMA\}/", ",", $term);
+
+			$out[] = $term;
+		}
+
+		return $out;
+	}
+
+	function search_transform_term($term){
+		$term = preg_replace("/(\s)/e", "'{WHITESPACE-'.ord('\$1').'}'", $term);
+		$term = preg_replace("/,/", "{COMMA}", $term);
+		return $term;
+	}
+
+	function search_escape_rlike($string){
+		return preg_replace("/([.\[\]*^\$])/", '\\\$1', $string);
+	}
+
+	function search_db_escape_terms($terms){
+		$out = array();
+		foreach($terms as $term){
+			$out[] = '[[:<:]]'.AddSlashes(search_escape_rlike($term)).'[[:>:]]';
+		}
+		return $out;
+	}
+
+
+
 	#
 	# get list of bugs
 	#
@@ -13,29 +88,47 @@
 	if ($_GET['assigned_to']){
 		$where = "status = 'open' AND assigned_user='" . addslashes($_GET['assigned_to']) ."'";
 		$title = 'Open Issues Assigned to ' . $_GET['assigned_to'];
+		if ($s){
+			$title .= " matching $s";
+		}
 	}
 	elseif ($_GET['opened_by']){
 		$where = "status != 'closed' AND opened_user='" . addslashes($_GET['opened_by']) ."'";
 		$title = 'Open & Resolved Issues reported by ' . $_GET['opened_by'];
+		if ($s){
+			$title .= " matching $s";
+		}
 	}
 	elseif ($_GET[all]){
 		$where = '1';
 		$title = 'All Issues';
+		if ($s){
+			$title = "Issues matching $s";
+		}
 	}
 	elseif ($_GET[resolved]){
 		$where = "status = 'resolved'";
 		$title = 'All Resolved Issues';
+		if ($s){
+			$title = "Resolved Issues matching $s";
+		}
 	}
 	elseif ($_GET[closed]){
 		$where = "status = 'closed'";
 		$title = 'All Closed Issues';
+		if ($s){
+			$title = "Closed Issues matching $s";
+		}
 	}else{
 		$where = "status = 'open'";
 		$title = 'All Open Issues';
+		if ($s){
+			$title = "Open Issues matching $s";
+		}
 	}
 
-	$count = db_fetch_single("SELECT COUNT(*) FROM bugs WHERE $where");
-	$bugs = db_fetch_all("SELECT * FROM bugs WHERE $where ORDER BY date_modified DESC LIMIT 100");
+	$count = db_fetch_single("SELECT COUNT(*) FROM bugs WHERE $where AND $search_extra");
+	$bugs = db_fetch_all("SELECT * FROM bugs WHERE $where AND $search_extra ORDER BY date_modified DESC LIMIT 100");
 
 	foreach ($bugs as $k => $v){
 
