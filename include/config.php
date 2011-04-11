3acc29d5ba681c9c00ca691d89ebff807a44a75e
Cal Henderson <cal@iamcal.com>
2011-04-11 12:47:29 -0700

    added an example config file

---
+++ include/config.php
@@ -0,0 +1,15 @@
+<?
+	$GLOBALS[cfg][db_host] = 'localhost';
+	$GLOBALS[cfg][db_user] = 'root';
+	$GLOBALS[cfg][db_pass] = 'root';
+	$GLOBALS[cfg][db_name] = 'bugs';
+
+	$GLOBALS[cfg][root_url]		= '/';
+	$GLOBALS[cfg][abs_root_url]	= 'http://bugs.company.com'.$GLOBALS[cfg][root_url];
+
+	$GLOBALS[cfg][max_attach_mb]	= 2;
+	
+	$GLOBALS[cfg][email_from_name]		= 'doug';
+	$GLOBALS[cfg][email_from_email]		= 'doug@company.com';
+	$GLOBALS[cfg][auto_email_args]		= '-fdoug@company.com';
+?>
